098865f382fb2368c92d80cbd5679315e3e7297c
Austin <austin.kispotta@gmail.com>
2020-04-23 13:34:31 +0530

    Added all moves

--- app.js
+++ app.js
@@ -12,16 +12,3 @@ app.get('/',(req,res) => {
 });
 
 app.listen('3000', () => console.log('Listening on port 3000'));
-
-// const hostname = 'localhost';
-// const port = 3000;
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello Worlld!\n');
-// });
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
